ae73fabe8d2a26a3e857b11bfb5367435119c954
Antonio Trento <antoniotrento83@gmail.com>
2016-11-23 20:25:44 +0100

    s

--- js/index.js
+++ js/index.js
@@ -7,35 +7,3 @@ var wintop = $(window).scrollTop(), docheight =
 
   		$('.scroll-line').css('width', (scrolled + '%'));
 });
-
-tinymce.init({
-  selector: '.demo textarea',
-  width: 754 - 2,
-  height: 372 - 111,
-  plugins: [
-    'advlist',
-    'autolink',
-    'lists',
-    'link',
-    'image',
-    'charmap',
-    'print',
-    'preview',
-    'anchor',
-    'searchreplace',
-    'visualblocks',
-    'code',
-    'fullscreen',
-    'insertdatetime',
-    'media',
-    'table',
-    'contextmenu',
-    'paste',
-    'imagetools'
-  ],
-  toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist | link image',
-  content_css: [
-    '//fast.fonts.net/cssapi/e6dc9b99-64fe-4292-ad98-6974f93cd2a2.css',
-    '//www.tinymce.com/css/codepen.min.css'
-  ]
-})
