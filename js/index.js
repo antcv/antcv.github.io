eb4b14c89cbb38c52cfc4023516bb6f991b67eb5
Antonio Trento <antoniotrento83@gmail.com>
2016-11-23 19:05:47 +0100

    s

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
-// There are a few JS dependancies
+/*-- Notes:
 
-// Check the settings to take a look
-// at those as they are necessary.
+Header IMG:   Change image by swapping out url in .blue-bg
+              Make solid color by removing 'background' properties
+Slant angle:  Modify skewY degree in .white-bg
+Top distance: Slant distance from top = margin in .white-bg
+              Text distance from top = margin in .content
+Fixed Header: 'turn off' with "position: absolute" in .blue-bg
+
+--*/
